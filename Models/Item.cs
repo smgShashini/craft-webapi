e74795a9d16cf412a0d28b2409224f04fee278f5
Shashini Maheshika Gunawardhana <smgunawardhana96@gmail.com>
2021-07-04 09:11:43 -0700

    Item model was added

---
+++ Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CraftWebApi.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+    }
+}
